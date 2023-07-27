fde6d9defb2c7034b3bd600ce8c84dcb2241adb2
SharmaShashank <117526251+dextroids@users.noreply.github.com>
2023-07-27 23:36:10 +0530

    Update Main.java
    Code is ready

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class Main {
-
+          public static void main(String []args)
+       {   
+         Product p = new Product();
+         p.product(5,9);
+         p.product(5,9,11);
+         p.product(5.11,5.33);
+       }
+  public class Product{
+        public int product(int x, int y) {
+             return x*y;
+        }
+        public int product(int x, int y, int z) {
+             return x*y*z;
+        } 
+     public double product(double x, double y) {
+       return x*y;
+     }
+    
+}
 }
